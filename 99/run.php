921cf0ab53ea79871c1c40588e0a0bcf74a63fdb
SanHayes <126791399+SanHayes@users.noreply.github.com>
2023-04-21 13:35:07 +0800

    Update run.php

--- 99/run.php
+++ 99/run.php
@@ -1,17 +1,18 @@
 <?php
-define('API_URL', 'http://127.0.0.1/99/api/api/');
+ini_set('max_execution_time', 0);
+define('API_URL', 'http://admin.827618.com/99/index/api/');
 while(true){
 	$t = time();
     if($t % 3 == 0){
-        @file_get_contents(API_URL . 'getdate');
-        @file_get_contents(API_URL . 'order');
-        @file_get_contents(API_URL . 'allotorder');
+        file_get_contents(API_URL . 'getdate');
+        file_get_contents(API_URL . 'order');
+        file_get_contents(API_URL . 'allotorder');
     }
     if($t % 30 == 0){
-        @file_get_contents(API_URL . 'checkbal');
+        file_get_contents(API_URL . 'checkbal');
     }
     if($t % 60 == 0){
-        @file_get_contents(API_URL . 'interest');
+        file_get_contents(API_URL . 'interest');
     }
     sleep(1);
 }
